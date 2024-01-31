9a296eba10624b315eea36da78bb23ba958748e1
Dramfi <kacper.saletra@gmail.com>
2024-01-31 13:09:49 +0100

    nowa metoda policz

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
-
+int policz(int a, int b){
+    return a+b;
+}
 int main() {
     printf("Hello, World!\n");
+    printf("W pierwszym branchu %d",policz(5,7));
     return 0;
 }
