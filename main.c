e4f8847af03afe1de1db900d0f541945dab323cc
Dramfi <kacper.saletra@gmail.com>
2024-01-31 13:15:38 +0100

    mnozenie w metodzie policz

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
-
+int policz(int a, int b){
+    return a*b;
+}
 int main() {
     printf("Hello, World!\n");
+    printf("W pierwszym branchu, 5*7 = %d",policz(5,7));
     return 0;
 }
